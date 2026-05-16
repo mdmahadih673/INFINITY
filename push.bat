72dece7eb56b89c9df1eea4a00a59490c266cf00
Md Mahadi <mdmahadih673@gmail.com>
2026-05-16 20:45:52 +0600

    Fix errors and remove node_modules for Vercel build

--- push.bat
+++ push.bat
@@ -3,16 +3,24 @@ echo ==========================================
 echo Uploading your project to GitHub...
 echo ==========================================
 
-git init
-git rm -r --cached node_modules 2>nul
-git add .
-git config user.email "mdmahadih673@gmail.com"
-git config user.name "Md Mahadi"
-git commit -m "Fix errors and remove node_modules for Vercel build"
-git remote add origin https://github.com/mdmahadih673/INFINITY.git 2>nul
-git remote set-url origin https://github.com/mdmahadih673/INFINITY.git
-git branch -M main
-git push -u origin main --force
+:: Try to find git path
+set "GIT_PATH=git"
+if exist "C:\Program Files\Git\cmd\git.exe" set "GIT_PATH=C:\Program Files\Git\cmd\git.exe"
+if exist "C:\Program Files\Git\bin\git.exe" set "GIT_PATH=C:\Program Files\Git\bin\git.exe"
+if exist "%LocalAppData%\Programs\Git\cmd\git.exe" set "GIT_PATH=%LocalAppData%\Programs\Git\cmd\git.exe"
+
+echo Using Git from: %GIT_PATH%
+
+"%GIT_PATH%" init
+"%GIT_PATH%" rm -r --cached node_modules 2>nul
+"%GIT_PATH%" add .
+"%GIT_PATH%" config user.email "mdmahadih673@gmail.com"
+"%GIT_PATH%" config user.name "Md Mahadi"
+"%GIT_PATH%" commit -m "Fix errors and remove node_modules for Vercel build"
+"%GIT_PATH%" remote add origin https://github.com/mdmahadih673/INFINITY.git 2>nul
+"%GIT_PATH%" remote set-url origin https://github.com/mdmahadih673/INFINITY.git
+"%GIT_PATH%" branch -M main
+"%GIT_PATH%" push -u origin main --force
 
 echo.
 echo ==========================================
